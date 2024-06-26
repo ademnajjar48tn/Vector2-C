7782f5bf0f758f9a45284f4385a29864868b2cf8
ademnajjar48 <cctestingportugal@gmail.com>
2024-06-26 21:56:18 +0100

    Creating kind of constructor for the struct Vector2

--- Vector2.h
+++ Vector2.h
@@ -2,3 +2,13 @@ typedef struct {
 	float x = 0;
 	float y = 0;
 }Vector2;
+
+struct Vector2* Vector2_new(int x = 0, int y = 0){
+	struct Vector2* result = (struct Vector2*) malloc(sizeof(struct Vector2));
+	
+	Vector2->x = 0;
+	Vector2->y = 0;
+
+	return result;
+}
+
